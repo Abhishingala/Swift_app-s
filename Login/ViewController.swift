ed58589fffbfaa613fe88b7c2c62f121a5c9deda
Abhishingala <abhishingala18@gmail.com>
2023-12-19 19:40:35 +0530

    Login_demo

---
+++ Login/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Abhi's Computers on 19/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    @IBOutlet weak var username_filed: UITextField!
+    
+    @IBOutlet weak var password_filed: UITextField!
+    
+    
+    
+    @IBAction func login_but(_ sender: UIButton) {
+        
+        let text_1 = "abhishingala"
+        let text_2 = "832058_8467"
+        
+        if username_filed.text == text_1{
+            
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
